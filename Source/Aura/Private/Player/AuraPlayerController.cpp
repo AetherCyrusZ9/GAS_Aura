4f18f7249954a319f352a1fdde1a25f8c5e21801
AetherCyrus <AetherCyrus@gmail.com>
2024-09-25 21:32:20 +0700

    Enhanced Input

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright AetherCyrus
+
+
+#include "Player/AuraaPlayerController.h"
+
---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright AetherCyrus
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraaPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraaPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
