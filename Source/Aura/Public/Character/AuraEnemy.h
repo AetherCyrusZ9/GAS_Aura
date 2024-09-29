73ad768ddcab9a074d589629d40eeb49e386aac7
AetherCyrus <AetherCyrus@gmail.com>
2024-09-29 17:52:47 +0700

    Init Ability Actor Info

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -16,7 +16,12 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase , public IEnemyInterface
 	GENERATED_BODY()
 public:
 	AAuraEnemy();
+
+	// Enemy Interface
 	void HighlightActor() override;
 	void UnHighlightActor() override;
+
+protected:
+	virtual void BeginPlay() override;
 	
 };
